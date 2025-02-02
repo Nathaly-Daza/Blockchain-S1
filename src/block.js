c9da01488391fe51637f68e9746016aca80ce665
AleLoops <113744119+Nathaly-Daza@users.noreply.github.com>
2025-02-02 16:59:07 -0500

    Test

--- src/block.js
+++ src/block.js
@@ -7,6 +7,47 @@ class Block{
         this.time = 0;
         this.previousBlockHash = '';
     }
+
+    validate(){
+        const self = this;
+        return new Promise((resolve, reject) => {
+            let currentHash = self.hash;
+
+            self.hash = SHA256(JSON.stringify({ ...self, hash: null})).toString();
+            if (currentHash !== self.hash) {
+                return resolve(false);
+            }
+            resolve(true);
+        });
+    }
+
+
+getBlockData(){
+    const self = this;
+    return new Promise((resolve, reject) => {
+        let encodedData = self.body;
+        let decodedData = hex2ascii(encodedData)
+        let dataObject = JSON.parse(decodedData);
+
+        if (dataObject === 'Genesis Block') {
+            reject(new Error('This is the Genesis Block'))
+        }
+
+        resolve(dataObject);
+
+    });
+ } 
+
+  toString(){
+    const { hash, height, body,time, previousBlockHash} = this;
+    return `Block -
+     hash: ${hash}, 
+     height: ${height}, 
+     body: ${body}, 
+     time: ${time}, 
+     previousBlockHash: ${previousBlockHash} 
+     ---------------------------------------------`
+  }
 }
 
-moduke.exports = Block;
+module.exports = Block;
